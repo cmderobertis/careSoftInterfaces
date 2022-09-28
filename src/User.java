6259836a8e8c6eaf152f5b53fd1f9186f37f4382
cmderobertis <cmderobertis@gmail.com>
2022-09-28 08:19:51 -0700

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,16 @@
+public class User {
+    protected Integer id;
+    protected int pin;
+
+    public User(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public int getPin() {
+        return this.pin;
+    }
+}
